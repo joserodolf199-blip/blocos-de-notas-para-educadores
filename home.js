0a3c2bc56c9ac30f337c5a8d98685c44fdf14f8f
Montanha0Fc <thiagoqn05@gmail.com>
2026-08-11 13:45:35 -0300

    Update and rename home.JS to home.js

---
+++ home.js
@@ -0,0 +1,32 @@
+// ========================================
+// FAQ - PERGUNTAS FREQUENTES
+// ========================================
+
+//Perguntas 
+const pergunta = document.querySelectorAll(".pergunta");
+
+perguntas.forEach((pergunta) => {
+
+//Elemento da pergunta
+const botaoPergunta = pergunta.querySelector(".botao-pergunta");
+const respostaPergunta = pergunta.querySelector(".resposta-pergunta");
+
+
+//Clique no botão
+botaoPergunta.addEventListener("click", () => {
+
+    //verificar se a pergunta já está pronta
+    const repostaAberta =
+        repostaPerguta.style.display === "block";
+
+
+    //Fechar todas as perguntas
+    pergunta.forEach((outrasPerguntas) => {
+
+        const outraResposta = 
+            outraPergunta.querySelector(".resposta-pergunta");
+        const outroBotao = 
+            outraPergunta.querySelecctor(".botao-pergunta");
+    }
+ }
+}
